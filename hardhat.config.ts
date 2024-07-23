f2b7d9edfac26fadcf0563ce7e9e929422a80d45
Auryn Macmillan <hi@auryn.xyz>
2024-07-23 11:56:38 -0400

    add sourcify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,6 +30,10 @@ const config: HardhatUserConfig = {
       url: process.env.GOERLI_URL || "",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   namedAccounts: {
     deployer: 0,
@@ -42,6 +46,9 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true,
   },
 }
 
